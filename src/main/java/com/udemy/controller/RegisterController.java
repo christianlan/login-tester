f1fff608d3d35d0883d171b2b7cd64baae1db54d
Christian Lan <christian.lan.external@atos.net>
2019-02-11 16:54:15 +0100

    adding some validations for registration

--- src/main/java/com/udemy/controller/RegisterController.java
+++ src/main/java/com/udemy/controller/RegisterController.java
@@ -17,10 +17,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.udemy.constant.ViewConstant;
 import com.udemy.entity.User;
 import com.udemy.entity.UserRole;
+import com.udemy.repository.UserRepository;
 import com.udemy.service.UserService2;
 import com.udemy.service.impl.UserService;
 
@@ -36,14 +38,19 @@ public class RegisterController {
 	@Qualifier("userService2Impl")
 	private UserService2 userService2;
 	
+	@Autowired
+	private UserRepository ur;
+	
 //	@Autowired
 //	private ICaptchaService captchaService;
 	
 	private static final Log LOG = LogFactory.getLog(RegisterController.class);
 	
 	@GetMapping("/register")
-	public String showRegisterForm(Model model) {
+	public String showRegisterForm(@RequestParam(name="message", required=false) String message, Model model) {
 		LOG.info("METHOD: showRegisterForm");
+		
+		model.addAttribute("message", message);
 
 		model.addAttribute("user", new User());
 		return ViewConstant.REGISTER;
@@ -52,39 +59,51 @@ public class RegisterController {
 	@PostMapping("/registerConfirmation")
 	public String ConfirmRegister(@ModelAttribute("user") User user, HttpServletRequest request, Model model) {
 
-		LOG.info("Register new user: " + user.toString());
-		
-		user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-		user.setEnabled(true);
-		
-		Set<UserRole> roles = new HashSet<UserRole>();
-		roles.add(new UserRole(user, "role_user"));
-		user.setUserRoles(roles);
-		
-		userService2.addUser(user);
-		
-		if (user.isUse2fa()) {
-			try {
-				String qrurl = userService.generateQRUrl(user);
-				model.addAttribute("qr", qrurl);
-				model.addAttribute("username", user.getUsername());
+		if (checkUsername(user.getUsername())) { // If the username doesn't exist
+			if (user.getPassword().equals(request.getParameter("password2"))) {
+				LOG.info("Register new user: " + user.toString());
 				
-				// return "redirect:/qrcode.html?lang=" + request.getLocale().getLanguage();
+				user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+				user.setEnabled(true);
 				
-				return ViewConstant.QR_CODE;
+				Set<UserRole> roles = new HashSet<UserRole>();
+				roles.add(new UserRole(user, "role_user"));
+				user.setUserRoles(roles);
 				
-			} catch (UnsupportedEncodingException e) {
-				// model.addAttribute("message", "Encoding not supported, 2fa disabled to the user");
-				// model.addAttribute("new_user", user.getUsername());
-				user.setUse2fa(false); // Forzar que no se use el 2fa				
-				return "redirect:/login?new_user=" + user.getUsername() + "?message=Encoding not supported, 2fa disabled to the user";
+				userService2.addUser(user);
+				
+				if (user.isUse2fa()) {
+					try {
+						String qrurl = userService.generateQRUrl(user);
+						model.addAttribute("qr", qrurl);
+						model.addAttribute("username", user.getUsername());
+						
+						// return "redirect:/qrcode.html?lang=" + request.getLocale().getLanguage();
+						
+						return ViewConstant.QR_CODE;
+						
+					} catch (UnsupportedEncodingException e) {
+						// model.addAttribute("message", "Encoding not supported, 2fa disabled to the user");
+						// model.addAttribute("new_user", user.getUsername());
+						user.setUse2fa(false); // Forzar que no se use el 2fa				
+						return "redirect:/login?new_user=" + user.getUsername() + "?message=Encoding not supported, 2fa disabled to the user";
+					}
+					//return "redirect:/register/qrcode?username=" + user.getUsername();
+				} else {
+					return "redirect:/login?new_user=" + user.getUsername();
+				}
+			} else {
+				return "redirect:/register/register?message=The repeated password is different from the password";
 			}
-			//return "redirect:/register/qrcode?username=" + user.getUsername();
 		} else {
-			return "redirect:/login?new_user=" + user.getUsername();
+			return "redirect:/register/register?message=The username " + user.getUsername() + " already exists";
 		}
 	}
 	
+	private boolean checkUsername(String username) {
+		User user = ur.findByUsername(username);
+		return user==null;
+	}
 	
 	
 //	@GetMapping("/qrcode")
