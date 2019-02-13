972b47eebe8bdac01754dd04678b16c681fdaa80
Christian Lan <christian.lan.external@atos.net>
2019-02-13 12:18:00 +0100

    Enhancement in the registration process, adding the qrcode validation and cancelling option (based on the 3rd approach)

--- src/main/java/com/udemy/controller/RegisterController.java
+++ src/main/java/com/udemy/controller/RegisterController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.jboss.aerogear.security.otp.Totp;
 import org.jboss.aerogear.security.otp.api.Base32;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -64,29 +65,24 @@ public class RegisterController {
 				user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
 				user.setEnabled(true);
 				
-				Set<UserRole> roles = new HashSet<UserRole>();
-				roles.add(new UserRole(user, "role_user"));
-				user.setUserRoles(roles);
-				
 				if (user.isUse2fa()) {
-					user.setSecret(Base32.random());
-				}
-				
-				userService2.addUser(user);
-				
-				if (user.isUse2fa()) {
+					user.setSecret(Base32.random()); //Generate and set the secret token
+					
 					try {
 						String qrurl = userService.generateQRUrl(user);
 						model.addAttribute("qr", qrurl);
-						model.addAttribute("username", user.getUsername());
+						model.addAttribute("user", user);
 												
 						return ViewConstant.QR_CODE;
 						
 					} catch (UnsupportedEncodingException e) {
-						user.setUse2fa(false); // Forzar que no se use el 2fa				
-						return "redirect:/login?new_user=" + user.getUsername() + "?message=Encoding not supported, 2fa disabled to the user";
+//						user.setUse2fa(false); // Forzar que no se use el 2fa				
+						return "redirect:/login?message=Encoding not supported, registration canceled automatically";
 					}
-				} else {
+				}
+				else {
+					setRolesForUser(user);
+					userService2.addUser(user); // Register the user to DB
 					return "redirect:/login?new_user=" + user.getUsername();
 				}
 			} else {
@@ -100,5 +96,56 @@ public class RegisterController {
 	private boolean checkUsername(String username) {
 		User user = ur.findByUsername(username);
 		return user==null;
+	}
+	
+	@PostMapping("/validatecode")
+	public String validateCode(HttpServletRequest request, Model model) {
+		String verificationCode = request.getParameter("code");
+		String secret = request.getParameter("secret");
+		
+		User user = new User();
+		user.setUsername(request.getParameter("username"));
+		user.setPassword(request.getParameter("password"));
+		user.setEnabled(new Boolean(request.getParameter("enabled")));
+		user.setUse2fa(new Boolean(request.getParameter("use2fa")));
+		user.setSecret(request.getParameter("secret"));
+		
+		Totp totp = new Totp(secret);
+		if (!isValidLong(verificationCode) || !totp.verify(verificationCode)) { // When the code is incorrect, go to failure url
+			model.addAttribute("error", true);
+			
+			String qrurl = request.getParameter("qr");
+			model.addAttribute("qr", qrurl);
+			
+			model.addAttribute("user", user);
+			
+			return ViewConstant.QR_CODE;
+		} else { 
+			setRolesForUser(user);
+			userService2.addUser(user); // Register the user to DB
+			return "redirect:/login?new_user=" + user.getUsername();
+		}
+	}
+	
+	@GetMapping("/cancel")
+	public String cancelRegistration(HttpServletRequest request) {
+//		String username = request.getParameter("username");
+//		LOG.info("Cancelling the registration for the user: " + username);
+		return "redirect:/login?message=You cancelled the registration";
+	}
+	
+	private boolean isValidLong(String code) {
+		try {
+			Long.parseLong(code);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
+	}
+	
+	private void setRolesForUser(User user) {
+		Set<UserRole> roles = new HashSet<UserRole>();
+		roles.add(new UserRole(user, "role_user"));
+		user.setUserRoles(roles);
 	}
 }
